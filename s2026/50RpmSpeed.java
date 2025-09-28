0da9f416e5ce610912ba6b48475b0b8469aeccc6
Hello19381 <krish.mathuria11@gmail.com>
2025-09-28 19:20:24 -0400

    Update 50RpmSpeed.java

--- s2026/50RpmSpeed.java
+++ s2026/50RpmSpeed.java
@@ -1,15 +1,13 @@
 package org.firstinspires.ftc.teamcode;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name = "Intake Wheels at 50 RPM", group = "Examples")
 public class RpmSpeed extends LinearOpMode {
-
     DcMotor intakeWheelR;
     DcMotor intakeWheelL;
-    double targetTicksPerSecond = 50;
+    double targetTicksPerSecond;
 
     @Override
     public void runOpMode() {
@@ -17,12 +15,16 @@ public class RpmSpeed extends LinearOpMode {
         intakeWheelR = hardwareMap.get(DcMotor.class, "intakeWheelR"); // M2
         intakeWheelL = hardwareMap.get(DcMotor.class, "intakeWheelL"); // M3
 
+        // Calculate target velocity in ticks per second
+        double targetRPM = 50;
+        double ticksPerRev = 560; // Example for REV HD Hex motor
+        targetTicksPerSecond = (targetRPM * ticksPerRev) / 60.0; // ≈ 466.67
+
         // Set mode to use encoder for velocity control
         intakeWheelR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         intakeWheelL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-       
-
+        waitForStart();
 
         while (opModeIsActive()) {
             if (gamepad2.x) {
@@ -34,19 +36,10 @@ public class RpmSpeed extends LinearOpMode {
                 intakeWheelR.setVelocity(0);
                 intakeWheelL.setVelocity(0);
             }
-
             telemetry.addData("Button Pressed", gamepad2.x);
             telemetry.addData("Right RPM", (intakeWheelR.getVelocity() * 60.0) / ticksPerRev);
             telemetry.addData("Left RPM", (intakeWheelL.getVelocity() * 60.0) / ticksPerRev);
             telemetry.update();
         }
-    }
-     
-     // Calculate target velocity in ticks per second
-    private setVelocity(targetTicksPerSecond){
-        double targetRPM = 50;
-        double ticksPerRev = 560; // Example for REV HD Hex motor
-        double targetTicksPerSecond = (targetRPM * ticksPerRev) / 60.0; // ≈ 466.67
-        waitForStart(); 
     }
 }
